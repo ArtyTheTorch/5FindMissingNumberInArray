812060aa91aa90151756d5f6dae93482f5666bcd
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 11:31:35 -0700

    Returns the index of the missing number on a array
    Input: String that represents a int array
    Example:  "1,2,3,4,,5,6"
    Output: the index where there is a null value : 4

---
+++ 5FindMissingNumberInArray/FindMissingNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5FindMissingNumberInArray
+{
+    class FindMissingNumber
+    {
+        string[] str;
+
+        public FindMissingNumber(string p)
+        {
+            str = p.Split(',');
+        }
+
+        public int getMissingNumber()
+        {
+            int pos=99999;
+            int temp;
+            for (int i = 0; i < str.Length; i++ )
+            {
+                if (!int.TryParse(str[i],out temp))// || str[i] == "")
+                {
+                    pos = i;
+                }
+            }
+            return pos;
+        }
+    }
+}
